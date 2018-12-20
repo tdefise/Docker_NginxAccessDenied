0e3e0d0b74621ddbf1311cc33c20e56a0f44f8d8
Thomas Defise <t.defise@gmail.com>
2018-12-20 16:06:11 +0100

    change capabilities

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@ git pull
 docker build -t tdefise/nginx .
 docker stop nginx
 docker rm nginx
-docker run --name=nginx      \
-           --detach=true          \
-           --publish=80:80/tcp  \
+docker run --name=nginx            \
+           --detach=true           \
+           --publish=80:80/tcp     \
+           --cap-drop=setpcap      \
+           --cap-drop=setfcap      \
+           --cap-drop=audit_write  \
+           --cap-drop=mknod        \
+           --cap-drop=fsetid       \
+           --cap-drop=fowner       \
            tdefise/nginx
